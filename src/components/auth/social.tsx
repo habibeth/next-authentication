b025d63e24a5fe33ecda78968b443cab61665fad
Ahsan Habib <habibstk97@gmail.com>
2024-02-28 10:10:08 +0600

    social login completed

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -1,16 +1,23 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
 import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react"
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
 
 
 const Social = () => {
+    const onClick = (provider: "google" | "github")=>{
+        signIn(provider, {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
+        })
+    }
     return (
         <div className="flex items-center justify-center gap-x-4 w-full">
             <Button
                 size={"lg"}
                 variant={"outline"}
                 className="w-full text-3xl"
-                onClick={()=>{}}
+                onClick={() => onClick("google")}
             >
                 <FcGoogle />
             </Button>
@@ -18,7 +25,7 @@ const Social = () => {
                 size={"lg"}
                 variant={"outline"}
                 className="w-full text-3xl"
-                onClick={()=>{}}
+                onClick={() => onClick("github")}
             >
                 <FaGithub />
             </Button>
